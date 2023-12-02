2c6db876139f57b22a73387b50e7c96d39e62baf
Casper Aangeenbrug <cas.aangeenbrug@gmail.com>
2023-12-02 22:29:11 +0100

    Solve day 2, part two

--- rust/2023/day-2/src/main.rs
+++ rust/2023/day-2/src/main.rs
@@ -18,11 +18,37 @@ fn main() {
         green_amount: 13,
         blue_amount: 14,
     };
+
     let possible_games_count = count_possible_games(&input, &bag_config);
     println!(
         "{} amount of games possible with this configuration",
         possible_games_count
     );
+
+    let power_sets_sum = get_power_sets_sum(&input);
+    println!("Sum of power sets is {}", power_sets_sum);
+}
+
+fn get_power_sets_sum(input: &String) -> u32 {
+    let game_strs: Vec<&str> = input.split("\n").filter(|line| !line.is_empty()).collect();
+    let games: Vec<Game> = game_strs
+        .iter()
+        .map(|s| {
+            let game_str = String::from(*s);
+            get_game_from_line(&game_str)
+        })
+        .collect();
+    let min_bags: Vec<Bag> = games.iter().map(|game| {
+        Bag {
+            red_amount: game.bags.iter().map(|bag| bag.red_amount).max().unwrap(),
+            blue_amount: game.bags.iter().map(|bag| bag.blue_amount).max().unwrap(),
+            green_amount: game.bags.iter().map(|bag| bag.green_amount).max().unwrap(),
+        }
+    }).collect();
+
+    min_bags.iter().map(|bag| {
+        bag.red_amount * bag.green_amount * bag.blue_amount
+    }).sum()
 }
 
 fn count_possible_games(input: &String, bag_config: &Bag) -> u32 {
@@ -99,7 +125,7 @@ fn extract_number(s: &str) -> u32 {
 
 #[cfg(test)]
 mod tests {
-    use crate::{count_possible_games, Bag};
+    use crate::{count_possible_games, Bag, get_power_sets_sum};
 
     #[test]
     fn test_count_possible_games() {
@@ -118,6 +144,21 @@ mod tests {
         };
         let actual = count_possible_games(&input, &bag_config);
         let expected = 8;
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_get_power_sets_sum() {
+        let input = String::from(
+            "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green\n
+            Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue\n
+            Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red\n
+            Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red\n
+            Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+        );
+
+        let actual = get_power_sets_sum(&input);
+        let expected = 2286;
         assert_eq!(actual, expected);
     }
 }
