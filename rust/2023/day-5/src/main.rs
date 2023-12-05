7caf44a21b30b667c6bf11ab1b94277bb4f3c50b
Casper Aangeenbrug <cas.aangeenbrug@gmail.com>
2023-12-05 20:01:07 +0100

    Solve day 5 part one

--- rust/2023/day-5/src/main.rs
+++ rust/2023/day-5/src/main.rs
@@ -1,108 +1,196 @@
 use regex::Regex;
-use std::{collections::HashMap, fs};
+use std::fs;
 
 struct Almanac<'a> {
-    seeds: Vec<u32>,
-    maps: Vec<SourceToDestMap<'a>>,
+    seeds: Vec<u64>,
+    plans: Vec<SeedingPlan<'a>>,
 }
 
-struct SourceToDestMap<'a> {
-    source_name: &'a str,
-    dest_name: &'a str,
-    source_to_dest: HashMap<&'a u32, &'a u32>,
+struct SeedingPlan<'a> {
+    name: &'a str,
+    mappings: Vec<Mapping>,
 }
 
-impl<'a> SourceToDestMap<'a> {
-    fn get_full_name(&self) -> String {
-        format!("{}-to-{}", self.source_name, self.dest_name)
+struct Mapping {
+    source_start: u64,
+    dest_start: u64,
+    range: u64,
+}
+
+impl Mapping {
+    fn get_destination(&self, source: u64) -> Option<u64> {
+        let is_in_range = source >= self.source_start && source <= self.source_start + self.range;
+        match is_in_range {
+            true => {
+                let delta = source - self.source_start;
+                let value = self.dest_start + delta;
+                Some(value)
+            },
+            false => None
+        }
+    }
+}
+
+impl<'a> SeedingPlan<'a> {
+    fn get_destination(&self, source: u64) -> u64 {
+        for mapping in &self.mappings {
+            let destination = mapping.get_destination(source);
+            if let Some(destination) = destination {
+                return destination;
+            }
+        }
+
+        source
+    }
+}
+
+impl<'a> Almanac<'a> {
+    fn get_seeding_plan(&self, source_name: &str, dest_name: &str) -> Result<&SeedingPlan, &'static str> {
+        let plans = self.plans.iter().filter(|plan| {
+            plan.name == format!("{}-to-{}", source_name, dest_name)
+        }).collect::<Vec<&SeedingPlan>>();
+
+        if plans.len() > 1 {
+            return Err("More than one seeding plan found");
+        }
+
+        let plan = *plans.first().ok_or("Failed to get plan")?;
+        Ok(plan)
     }
 }
 
 fn main() {
     let input = fs::read_to_string("data.txt").unwrap();
-    deserialize_input(&input);
+    let sanitized_input = input.trim().to_string();
+    let almanac = deserialize_input(&sanitized_input);
+
+    let lowest_soil_number = get_lowest_soil_number(&almanac).unwrap();
+    println!("Lowest soil number is {}", lowest_soil_number);
 }
 
-fn deserialize_input(input: &String) -> Result<Almanac, &'static str> {
-    let sanitized_input = input.trim().to_string();
+fn get_lowest_soil_number(almanac: &Almanac) -> Result<u64, &'static str> {
+    let mut locations: Vec<u64> = vec![];
+    for seed in almanac.seeds.iter() {
+        let soil = almanac.get_seeding_plan("seed", "soil")?.get_destination(*seed);
+        let fertilizer = almanac.get_seeding_plan("soil", "fertilizer")?.get_destination(soil);
+        let water = almanac.get_seeding_plan("fertilizer", "water")?.get_destination(fertilizer);
+        let light = almanac.get_seeding_plan("water", "light")?.get_destination(water);
+        let temperature = almanac.get_seeding_plan("light", "temperature")?.get_destination(light);
+        let humidity = almanac.get_seeding_plan("temperature", "humidity")?.get_destination(temperature);
+        let location = almanac.get_seeding_plan("humidity", "location")?.get_destination(humidity);
+        locations.push(location);
+    }
+
+    let min = locations.iter().min().ok_or("Failed to get the lowest value")?;
+    Ok(*min)
+}
+
+fn deserialize_input(sanitized_input: &String) -> Almanac {
     let groups = sanitized_input.split("\n\n").collect::<Vec<&str>>();
 
-    let seeds_str = groups.get(0).ok_or("Failed to obtain seeds")?;
+    let seeds_str = groups.get(0).expect("Failed to obtain seeds");
     let seeds = get_numbers(&seeds_str.to_string());
 
     let map_groups = groups[1..].to_vec();
-    let maps: Vec<SourceToDestMap> = map_groups.iter().map(|map_group| {
+    let plans: Vec<SeedingPlan> = map_groups.iter().map(|map_group| {
         let lines = map_group.lines().collect::<Vec<&str>>();
         let name_line = lines.get(0).unwrap();
-        let source_name = get_source_name(name_line);
-        let dest_name = get_dest_name(name_line);
+        let name = get_name(name_line).expect("Failed to get name");
 
-        let number_lines = lines[1..].iter().collect::<Vec<>>();
+        let number_lines = lines[1..].iter().collect::<Vec<&&str>>();
+        let mappings: Vec<Mapping> = number_lines.iter().map(|line| {
+            let numbers = get_numbers(&line.to_string());
+            let dest_start = numbers.get(0).expect("Failed to get destination start");
+            let source_start = numbers.get(1).expect("Failed to get source start");
+            let range = numbers.get(2).expect("Failed to get range");
+
+            Mapping {
+                dest_start: *dest_start,
+                range: *range,
+                source_start: *source_start,
+            }
+        }).collect();
+
+        SeedingPlan {
+            name,
+            mappings,
+        }
     }).collect();
 
-    Ok(Almanac {
+    Almanac {
         seeds,
-        maps
-    })
+        plans,
+    }
 }
 
-fn get_source_name(name_line: &str) -> &str {
-    todo!()
+fn get_name(s: &str) -> Option<&str> {
+    let re = Regex::new(r"\b(\w+-to-\w+)\b").unwrap();
+    re
+        .captures_iter(s)
+        .map(|c| c.extract::<1>().0)
+        .collect::<Vec<&str>>()
+        .first()
+        .map(|v| *v)
 }
 
-fn get_dest_name(name_line: &str) -> &str {
-    todo!()
-}
-
-fn get_numbers(s: &String) -> Vec<u32> {
-    let number_re = Regex::new(r"(\d+)").unwrap();
-    number_re
+fn get_numbers(s: &String) -> Vec<u64> {
+    let re = Regex::new(r"(\d+)").unwrap();
+    re
         .captures_iter(s)
         .map(|c| {
             let value = c.extract::<1>().0;
-            value.parse::<u32>().unwrap()
+            value.parse::<u64>().unwrap()
         })
         .collect()
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::{deserialize_input, get_lowest_soil_number};
+
     const INPUT: &str = "
-    seeds: 79 14 55 13
+        seeds: 79 14 55 13
 
-    seed-to-soil map:
-    50 98 2
-    52 50 48
+        seed-to-soil map:
+        50 98 2
+        52 50 48
 
-    soil-to-fertilizer map:
-    0 15 37
-    37 52 2
-    39 0 15
+        soil-to-fertilizer map:
+        0 15 37
+        37 52 2
+        39 0 15
 
-    fertilizer-to-water map:
-    49 53 8
-    0 11 42
-    42 0 7
-    57 7 4
+        fertilizer-to-water map:
+        49 53 8
+        0 11 42
+        42 0 7
+        57 7 4
 
-    water-to-light map:
-    88 18 7
-    18 25 70
+        water-to-light map:
+        88 18 7
+        18 25 70
 
-    light-to-temperature map:
-    45 77 23
-    81 45 19
-    68 64 13
+        light-to-temperature map:
+        45 77 23
+        81 45 19
+        68 64 13
 
-    temperature-to-humidity map:
-    0 69 1
-    1 0 69
+        temperature-to-humidity map:
+        0 69 1
+        1 0 69
 
-    humidity-to-location map:
-    60 56 37
-    56 93 4
+        humidity-to-location map:
+        60 56 37
+        56 93 4
     ";
 
     #[test]
-    fn test() {}
+    fn test() {
+        let sanitized_input = INPUT.trim().to_string();
+        let almanac = deserialize_input(&sanitized_input);
+
+        let actual = get_lowest_soil_number(&almanac).unwrap();
+        let expected: u64 = 35;
+        assert_eq!(actual, expected);
+    }
 }
